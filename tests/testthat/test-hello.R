1843cc4c4af618e8d85c68f0496dbd972b898c6e
Ellis Hughes <ellis.h.hughes@gsk.com>
2021-10-05 09:53:45 -0700

    explicit binary file comparison

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,10 +1,15 @@
 test_that("png generation works", {
-  expect_snapshot_file(save_as_image({
+
+  save_as_image({
     plot(c(1, 2, 3, 4))
   },
   device = png,
   file = file.path(tempdir(),"test.png")
-  ))
+  )
+
+
+
+  expect_true(compare_file_binary(file.path(tempdir(),"test.png"), "_snaps/hello/test.png"))
 })
 
 test_that("jpeg generation works", {
